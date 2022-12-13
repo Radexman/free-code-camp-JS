cc1d8ae3126c10521bded0378fdaccb4eb94e9a2
Radosław Siek <106437063+Radexman@users.noreply.github.com>
2022-12-13 20:45:35 +0100

    12.13

--- app.js
+++ app.js
@@ -1,8 +1,56 @@
-let anyName = 'Emilia';
+// =========================================== String Methods =============================================== //
 
-function getRandomChar(name) {
-    let randomChar = name.charAt(Math.floor(Math.random() * name.length));
-    return randomChar;
-}
+// const myVariable = 'Mathematics';
 
-console.log(getRandomChar(anyName));
+// The length property
+// console.log(myVariable.length);
+
+// Returns character at provided position
+// console.log(myVariable.charAt(6));
+
+// Returns the first position of passed value
+// console.log(myVariable.indexOf('Mat'));
+
+// Returns position of the last occurence
+// console.log(myVariable.lastIndexOf('ath'));
+
+// Takes starting and ending indexes and provides characters in between
+// console.log(myVariable.slice(0, 4));
+
+// Turns string to upper characters
+// console.log(myVariable.toUpperCase());
+
+// Turns string to lower characters
+// console.log(myVariable.toLowerCase());
+
+// If the chars are not inside the string it returns false
+// console.log(myVariable.includes('div'));
+
+// Splits string in provided index
+// console.log(myVariable.split('e'));
+
+// =========================================== Number Methods =============================================== //
+
+// An intiger is a whole nummber
+// const myNumber = 42;
+
+// A number with a decimal point is a float which
+// references the "floating point"
+// const myFloat= 42.02;
+// const myString = '42.123';
+
+// Turns a string into number
+// console.log(Number(myString) === myNumber);
+// console.log(Number(false));
+
+// Determines wether the passed value is an integer
+// console.log(Number.isInteger(myNumber));
+
+// Parses an argument and returns floating point number
+// If a number cannot be parsed from the argument, it returns NaN
+// console.log(Number.parseFloat(myString));
+
+// Formats a number according to how many decimal points
+// you provide as the parameter
+// console.log(Number.parseFloat(myString).toFixed(2));
+
